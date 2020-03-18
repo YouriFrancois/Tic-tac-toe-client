5121b7aace1bc1ceaf11a944f8e5047b9cb105fc
YouriFrancois <yourifrancois97@gmail.com>
2020-03-17 23:02:38 -0400

    better ai

--- assets/scripts/play/ai.js
+++ assets/scripts/play/ai.js
@@ -4,6 +4,10 @@ const aiMove = function(arr){
 let ran =''
 
 if(arr[4]===''){return 4}
+else if(arr[0]===''){return 0}
+else if(arr[2]===''){return 2}
+else if(arr[6]===''){return 6}
+else if(arr[8]===''){return 8}
 else {
 const ar1 =  arr.findIndex(num=> num==="")
 console.log(" ar1 "+ar1)
